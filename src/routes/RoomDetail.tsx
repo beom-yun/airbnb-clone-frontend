843c2611fdb0faa87dc325798209abdcadaf73f2
beom-yun <scds0326@naver.com>
2022-11-01 12:18:40 +0900

    Reviews

--- src/routes/RoomDetail.tsx
+++ src/routes/RoomDetail.tsx
@@ -1,19 +1,17 @@
-import {
-  Box,
-  Grid,
-  GridItem,
-  Heading,
-  Image,
-  Skeleton,
-} from '@chakra-ui/react';
+import { Avatar, Box, Grid, GridItem, Heading, HStack, Image, Skeleton, Text, VStack } from '@chakra-ui/react';
 import { useQuery } from '@tanstack/react-query';
+import { FaStar } from 'react-icons/fa';
 import { useParams } from 'react-router-dom';
-import { getRoom } from '../api';
-import { IRoomDetail } from '../types';
+import { getRoom, getRoomReviews } from '../api';
+import { IReview, IRoomDetail } from '../types';
 
 export default function RoomDetail() {
   const { roomPk } = useParams();
   const { isLoading, data } = useQuery<IRoomDetail>(['rooms', roomPk], getRoom);
+  const { data: reviewsData, isLoading: isReviewsLoading } = useQuery<IReview[]>(
+    ['rooms', roomPk, 'reviews'],
+    getRoomReviews
+  );
   return (
     <Box mt={10} px={{ base: 10, lg: 40 }}>
       <Skeleton height={'43px'} width="50%" isLoaded={!isLoading}>
@@ -29,21 +27,41 @@ export default function RoomDetail() {
         templateColumns={'repeat(4, 1fr)'}
       >
         {[0, 1, 2, 3, 4].map(index => (
-          <GridItem
-            colSpan={index === 0 ? 2 : 1}
-            rowSpan={index === 0 ? 2 : 1}
-            overflow={'hidden'}
-            key={index}
-          >
-            <Image
-              objectFit={'cover'}
-              w="100%"
-              h="100%"
-              src={data?.photos[index].file}
-            />
+          <GridItem colSpan={index === 0 ? 2 : 1} rowSpan={index === 0 ? 2 : 1} overflow={'hidden'} key={index}>
+            <Image objectFit={'cover'} w="100%" h="100%" src={data?.photos[index].file} />
           </GridItem>
         ))}
       </Grid>
+      <HStack mt={10} justifyContent={'space-between'} width="100%">
+        <VStack alignItems={'flex-start'}>
+          <Skeleton isLoaded={!isLoading} height="30px">
+            <Heading fontSize={'2xl'}>House hosted by {data?.owner.name}</Heading>
+          </Skeleton>
+          <Skeleton isLoaded={!isLoading} height="30px">
+            <HStack justifyContent={'flex-start'} w="100%">
+              <Text>
+                {data?.toilets} toilet{data?.toilets === 1 ? '' : 's'}
+              </Text>
+              <Text>∙</Text>
+              <Text>
+                {data?.rooms} room{data?.rooms === 1 ? '' : 's'}
+              </Text>
+            </HStack>
+          </Skeleton>
+        </VStack>
+        <Avatar name={data?.owner.name} size={'xl'} src={data?.owner.avatar} />
+      </HStack>
+      <Box mt={10}>
+        <Heading fontSize={'2xl'}>
+          <HStack>
+            <FaStar /> <Text>{data?.rating}</Text>
+            <Text>∙</Text>
+            <Text>
+              {reviewsData?.length} review{reviewsData?.length === 1 ? '' : 's'}
+            </Text>
+          </HStack>
+        </Heading>
+      </Box>
     </Box>
   );
 }
