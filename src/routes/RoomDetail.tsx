78d0c6237d7dfc7454e2fdd3aca1da87d7421615
beom-yun <scds0326@naver.com>
2022-11-03 14:04:08 +0900

    Conclusions

--- src/routes/RoomDetail.tsx
+++ src/routes/RoomDetail.tsx
@@ -1,4 +1,16 @@
-import { Avatar, Box, Grid, GridItem, Heading, HStack, Image, Skeleton, Text, VStack } from '@chakra-ui/react';
+import {
+  Avatar,
+  Box,
+  Container,
+  Grid,
+  GridItem,
+  Heading,
+  HStack,
+  Image,
+  Skeleton,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
 import { useQuery } from '@tanstack/react-query';
 import { FaStar } from 'react-icons/fa';
 import { useParams } from 'react-router-dom';
@@ -52,7 +64,7 @@ export default function RoomDetail() {
         <Avatar name={data?.owner.name} size={'xl'} src={data?.owner.avatar} />
       </HStack>
       <Box mt={10}>
-        <Heading fontSize={'2xl'}>
+        <Heading mb={5} fontSize={'2xl'}>
           <HStack>
             <FaStar /> <Text>{data?.rating}</Text>
             <Text>∙</Text>
@@ -61,6 +73,25 @@ export default function RoomDetail() {
             </Text>
           </HStack>
         </Heading>
+        <Container mt={15} maxW="container.lg" marginX="none">
+          <Grid gap={10} templateColumns={'1fr 1fr'}>
+            {reviewsData?.map((review, index) => (
+              <VStack alignItems={'flex-start'} key={index}>
+                <HStack>
+                  <Avatar name={review.user.name} src={review.user.avatar} size="md" />
+                  <VStack spacing={0} alignItems={'flex-start'}>
+                    <Heading fontSize={'md'}>{review.user.name}</Heading>
+                    <HStack spacing={1}>
+                      <FaStar size="12px" />
+                      <Text>{review.rating}</Text>
+                    </HStack>
+                  </VStack>
+                </HStack>
+                <Text>{review.payload}</Text>
+              </VStack>
+            ))}
+          </Grid>
+        </Container>
       </Box>
     </Box>
   );
