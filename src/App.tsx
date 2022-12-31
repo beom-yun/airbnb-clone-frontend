17a87bff70c754b8ddcf6deefd9c31d2e9fb0f73
beom-yun <scds0326@naver.com>
2023-01-01 02:11:24 +0900

    #17.2 Setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Text } from '@chakra-ui/react';
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <Text color={'red.600'} fontSize={'6xl'}>
+        It works!
+      </Text>
+    </div>
+  );
+}
+
+export default App;
