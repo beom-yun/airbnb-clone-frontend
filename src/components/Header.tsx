ed1380c07e35d9149c8cbac4a4e2f13cd933e886
beom-yun <scds0326@naver.com>
2023-01-03 13:24:48 +0900

    #18.3 Dark Mode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,14 @@
-import { Box, Button, HStack, IconButton, useDisclosure } from '@chakra-ui/react';
-import { FaAirbnb, FaMoon } from 'react-icons/fa';
+import {
+  Box,
+  Button,
+  HStack,
+  IconButton,
+  LightMode,
+  useColorMode,
+  useColorModeValue,
+  useDisclosure,
+} from '@chakra-ui/react';
+import { FaAirbnb, FaMoon, FaSun } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import LoginModal from './LoginModal';
 import SignUpModal from './SignUpModal';
@@ -7,21 +16,26 @@ import SignUpModal from './SignUpModal';
 export default function Header() {
   const { isOpen: isLoginOpen, onClose: onLoginClose, onOpen: onLoginOpen } = useDisclosure();
   const { isOpen: isSignUpOpen, onClose: onSignUpClose, onOpen: onSignUpOpen } = useDisclosure();
+  const { toggleColorMode } = useColorMode();
+  const logoColor = useColorModeValue('red.500', 'red.300');
+  const Icon = useColorModeValue(FaMoon, FaSun);
 
   return (
     <HStack py={5} px={10} borderBottomWidth={'1px'} justifyContent={'space-between'}>
-      <Box color={'red.500'}>
+      <Box color={logoColor}>
         <Link to={'/'}>
           <FaAirbnb size={48} />
         </Link>
       </Box>
 
       <HStack spacing={2}>
-        <IconButton aria-label="Toggle dark mode" icon={<FaMoon />} variant={'ghost'} />
+        <IconButton onClick={toggleColorMode} aria-label="Toggle dark mode" icon={<Icon />} variant={'ghost'} />
         <Button onClick={onLoginOpen}>Log in</Button>
-        <Button onClick={onSignUpOpen} colorScheme={'red'}>
-          Sign up
-        </Button>
+        <LightMode>
+          <Button onClick={onSignUpOpen} colorScheme={'red'}>
+            Sign up
+          </Button>
+        </LightMode>
       </HStack>
       <LoginModal isOpen={isLoginOpen} onClose={onLoginClose} />
       <SignUpModal isOpen={isSignUpOpen} onClose={onSignUpClose} />
